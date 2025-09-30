6e9509c5c81dd587b58121100c84d26119e6db2f
Mandar712 <mandargujalwar1@gmail.com>
2025-09-30 19:09:47 +0530

    Adding all the libraries that we will be needing to build this game.

---
+++ tictactoe.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+
+void drawboard(char *spaces);
+void playermove(char *spaces,char player);
+void computermove(char *spaces, char computer);
+bool checkwinner(char *spaces, char player, char computer);
+bool checktie(char *spaces);
+
+int main(){
+
+    
+    return 0;
+}
+
+
+
+
+void drawboard(char *spaces){
+
+}
+void playermove(char *spaces,char player){
+
+}
+void computermove(char *spaces, char computer){
+
+}
+bool checkwinner(char *spaces, char player, char computer){
+
+}
+bool checktie(char *spaces);{
+    
+}
