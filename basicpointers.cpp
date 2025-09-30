ec2882d59e8791d1eafb99059a48e3705d44f5eb
Mandar712 <mandargujalwar1@gmail.com>
2025-09-30 19:04:08 +0530

    Added a new code null pointer checker

--- basicpointers.cpp
+++ basicpointers.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 
-int main(){
-    std::string wot = "Bruh";
-    int age = 22;
-    std::string *pwot = &wot;
-    int *page = &age;
+// int main(){
+//     std::string wot = "Bruh";
+//     int age = 22;
+//     std::string *pwot = &wot;
+//     int *page = &age;
     
-    std::cout << *pwot << std::endl;
-    std::cout << *page << std::endl;
+//     std::cout << *pwot << std::endl;
+//     std::cout << *page << std::endl;
     
+//     return 0;
+// }
+int main() {
+    int *pointer = nullptr;
+    int x = 123;
+    pointer = &x;
+
+    if (pointer = nullptr) {
+        std::cout << "address was not assigned!\n";
+
+    }else {
+        std::cout << "Address was assigned! \n";
+    }
     return 0;
 }
