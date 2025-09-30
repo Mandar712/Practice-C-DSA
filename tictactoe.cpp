653a411fc74d61221338ea499c849cfe4f9ac7ed
Mandar712 <mandargujalwar1@gmail.com>
2025-09-30 19:28:34 +0530

    drawboard function is done

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -8,15 +8,27 @@ bool checkwinner(char *spaces, char player, char computer);
 bool checktie(char *spaces);
 
 int main(){
+    char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
 
-    
+    drawboard(spaces);
+
     return 0;
 }
 
-
-
-
 void drawboard(char *spaces){
+    std::cout << "     |     |     " << std::endl;
+    std::cout << "  " << spaces[0]<<"  |  "<< spaces[1] << "  |  "<<spaces[2]<<"  " << std::endl;
+    std::cout << "_____|_____|_____" << std::endl;
+    std::cout << "     |     |     " << std::endl;
+    std::cout << "  " << spaces[3]<<"  |  "<< spaces[4] << "  |  "<<spaces[5]<<"  " << std::endl;
+    std::cout << "_____|_____|_____" << std::endl;
+    std::cout << "     |     |     " << std::endl;
+    std::cout << "  " << spaces[6]<<"  |  "<< spaces[7] << "  |  "<<spaces[8]<<"  " << std::endl;
+    std::cout << "     |     |     " << std::endl;
+    std::cout << std::endl;
 
 }
 void playermove(char *spaces,char player){
@@ -26,8 +38,8 @@ void computermove(char *spaces, char computer){
 
 }
 bool checkwinner(char *spaces, char player, char computer){
-
+    return 0;
 }
-bool checktie(char *spaces);{
-    
+bool checktie(char *spaces){
+    return 0;
 }
